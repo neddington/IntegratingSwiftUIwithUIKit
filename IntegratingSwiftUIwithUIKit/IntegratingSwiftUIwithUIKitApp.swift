b6f123bdb71ad41170c28b84207cee582260cfad
neddington <neddington@cascadia.edu>
2023-12-11 14:46:49 -0800

    Initial Commit

---
+++ IntegratingSwiftUIwithUIKit/IntegratingSwiftUIwithUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegratingSwiftUIwithUIKitApp.swift
+//  IntegratingSwiftUIwithUIKit
+//
+//  Created by Eddington, Nick on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IntegratingSwiftUIwithUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
