cc8feda6cc2049e20126dc1c765cfc10a11008f6
neddington <neddington@cascadia.edu>
2023-12-11 14:57:00 -0800

    Fin

--- IntegratingSwiftUIwithUIKit/ContentView.swift
+++ IntegratingSwiftUIwithUIKit/ContentView.swift
@@ -1,24 +1,64 @@
-//
-//  ContentView.swift
-//  IntegratingSwiftUIwithUIKit
-//
-//  Created by Eddington, Nick on 12/11/23.
-//
-
 import SwiftUI
+import PDFKit
+import WebKit
+
+struct PDFView: UIViewRepresentable {
+    var pdfName: String // Name of the PDF file
+
+    func makeUIView(context: Context) -> PDFKit.PDFView {
+        let pdfView = PDFKit.PDFView()
+        if let pdfURL = Bundle.main.url(forResource: pdfName, withExtension: "pdf") {
+            if let document = PDFDocument(url: pdfURL) {
+                pdfView.document = document
+            }
+        }
+        return pdfView
+    }
+
+    func updateUIView(_ uiView: PDFKit.PDFView, context: Context) {}
+}
+
+struct WebView: UIViewRepresentable {
+    var urlString: String // URL of the webpage
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            // First Tab - Display PDF
+            VStack {
+                PDFView(pdfName: "GoofyAdventures011(1991)") // Replace with your PDF file name
+                    .edgesIgnoringSafeArea(.all)
+            }
+            .tabItem {
+                Label("PDF", systemImage: "doc.richtext.fill")
+            }
+
+            // Second Tab - Display Webpage
+            VStack {
+                WebView(urlString: "https://www.disney.com") // Replace with your URL
+                    .edgesIgnoringSafeArea(.all)
+            }
+            .tabItem {
+                Label("Web", systemImage: "globe")
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
